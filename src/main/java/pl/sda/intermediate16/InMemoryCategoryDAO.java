5a860402c546a7f1b56e9aa838a2da81d218cafa
Krzyyy <k.falinski@sdacademy.pl>
2019-03-23 15:33:42 +0100

    parsing categories

--- src/main/java/pl/sda/intermediate16/InMemoryCategoryDAO.java
+++ src/main/java/pl/sda/intermediate16/InMemoryCategoryDAO.java
@@ -5,14 +5,17 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class InMemoryCategoryDAO {
 
     private List<Category> categoryList = new ArrayList<>();
+
+    public InMemoryCategoryDAO() {
+        initializeCategories();
+    }
 
     private List<String> loadCategoriesFromFile() {
         ClassLoader classLoader = this.getClass().getClassLoader();
@@ -38,6 +41,48 @@ public class InMemoryCategoryDAO {
                     return category;
                 })
                 .collect(Collectors.toList());
+
+        Map<Integer, List<Category>> categoriesMap =
+                categories.stream()
+                        .collect(Collectors.groupingBy(
+                                c -> calculateDepth(c.getCategoryName())));
+
+//        for (Category category : categories) {
+//            Integer depth = calculateDepth(category.getCategoryName());
+//            if (categoriesMap.containsKey(depth)) {
+//                categoriesMap.get(depth).add(category);
+//            } else {
+//                List<Category> depthCategoryList = new ArrayList<>();
+//                depthCategoryList.add(category);
+//                categoriesMap.put(depth,depthCategoryList);
+//            }
+//        }
+    populateParentId(categoriesMap,0);
+    categoryList = categories;
+    }
+
+    private void populateParentId(Map<Integer, List<Category>> categoriesMap, int depth) {
+        List<Category> children = categoriesMap.get(depth);
+        children.stream()
+                .forEach(c -> {
+                    List<Category> potentialParents = categoriesMap.get(depth - 1);
+                    Integer parentId = potentialParents.stream()
+                            .map(Category::getId)
+                            .filter(id -> id < c.getId())
+                            .sorted((a, b) -> b - a)
+                            .findFirst()
+                            .orElse(null);
+                    c.setParentId(parentId);
+                });
+        if (categoriesMap.containsKey(depth + 1)){
+            populateParentId(categoriesMap,depth + 1);
+        }
+
+    }
+
+
+    private Integer calculateDepth(String categoryName) {
+        return categoryName.split("\\S+")[0].length();
     }
 
 }
