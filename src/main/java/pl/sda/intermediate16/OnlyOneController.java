29a2a5a0174e0a58b91a767c62ba7db1cfda494e
Krzyyy <k.falinski@sdacademy.pl>
2019-03-29 19:41:04 +0100

    heroku

--- src/main/java/pl/sda/intermediate16/OnlyOneController.java
+++ src/main/java/pl/sda/intermediate16/OnlyOneController.java
@@ -3,11 +3,19 @@ package pl.sda.intermediate16;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class OnlyOneController { //ta klasa pozwala kontaktować się przeglądarce z naszą aplikacją
 
     CategorySearchService categorySearchService = new CategorySearchService();
+
+
+    @ResponseBody
+    @RequestMapping("/")
+    public String ok (){
+        return "OK";
+    }
 
     @RequestMapping("/categories") //pod takim urlem dostępna jest strona z kategoriami
     public String categories(String searchText, Model model){
