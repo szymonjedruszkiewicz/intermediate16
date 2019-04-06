ab9bdb2cfd9f19e8e54685f4f29e5eb5753665fb
Krzyyy <k.falinski@sdacademy.pl>
2019-04-06 13:17:14 +0200

    regexes

--- src/main/java/pl/sda/intermediate16/users/UserValidationService.java
+++ src/main/java/pl/sda/intermediate16/users/UserValidationService.java
@@ -7,9 +7,33 @@ public class UserValidationService {
     public Map<String, String> validate(UserRegistrationDTO userRegistrationDTO){
         Map<String, String> errorMap = new HashMap<>();
         String firstName = userRegistrationDTO.getFirstName();
-        if (firstName.length() <3){
+        String lastName = userRegistrationDTO.getLastName();
+        String email = userRegistrationDTO.getEMail();
+        String zipCode = userRegistrationDTO.getZipCode();
+        String birthDate = userRegistrationDTO.getBirthDate();
+        String phone = userRegistrationDTO.getPhone();
+
+
+        if (firstName == null || !firstName.matches("[A-Z][a-z]{2,}")){
             errorMap.put("firstNameValRes", "Powinny być przynajmniej 3 znaki");
         }
+        if (lastName == null || !lastName.matches("^[A-Z][a-z]{2,}(-[A-Z][a-z]*)?$")){
+            errorMap.put("lastNameValRes", "Przynajmniej 3 znaki oraz pierwsza duża, reszta małe \n" +
+                    "\t+ dopuszczenie \"-\" i drugiego członu z dużej litery");
+        }
+        if (email == null || !email.matches("^[\\w\\.]{2,}@([a-z]{2,}\\.){1,2}[a-z]{2,}")){
+            errorMap.put("emailValRes", "inny jest, mowie ci");
+        }
+        if (zipCode == null || !zipCode.matches("^[0-9]{2}-[0-9]{3}")) {
+            errorMap.put("zipCodeValRes", "Kod pocztowy powinien mieć format 12-345.");
+        }
+        if (birthDate == null || !birthDate.matches("^(19|20)[0-9]{2}-((0[1-9])|(1[0-2]))-((0[1-9])|([12][0-9])|(3[01]))$")) {
+            errorMap.put("birthDateValRes", "no chyba nie");
+        }
+        if (phone == null || !phone.matches("")) {
+            errorMap.put("phoneValRes", "no chyba nie");
+        }
+
         return errorMap;
     }
 }
